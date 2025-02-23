f8c588ca0c130fbf001f3a03be06d90b6f9e42c2
Ramiro Benitez <rbenitez@gottert.com.ar>
2025-02-23 16:23:48 -0300

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -16,6 +16,7 @@ export default function AppNavigator() {
         headerTitleStyle: {
           fontWeight: "bold",
         },
+        headerTitleAlign: "center",
       }}
     >
       <Stack.Screen
