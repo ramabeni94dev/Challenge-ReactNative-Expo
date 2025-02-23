2565e3eff9a8df143d8a9bb1bc8f2abb6a7e8c1a
Ramiro Benitez <rbenitez@gottert.com.ar>
2025-02-23 20:09:32 -0300

    Cambio de nombre de variables de  entorno

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-import { EXPO_API_KEY, EXPO_API_URL } from "@env";
+import { EXPO_PUBLIC_API_KEY, EXPO_PUBLIC_API_URL } from "@env";
 
-const API_KEY = `${EXPO_API_KEY}`;
+const API_KEY = `${EXPO_PUBLIC_API_KEY}`;
+const API_URL = `${EXPO_PUBLIC_API_URL}`;
 
 export const getPageSpeedData = async (url, selectedCategories, strategy) => {
   const categoryParams = selectedCategories
     .map((cat) => `&category=${cat}`)
     .join("");
 
-  const endpoint = `${EXPO_API_URL}?url=${encodeURIComponent(
+  const endpoint = `${API_URL}?url=${encodeURIComponent(
     url
   )}&key=${API_KEY}${categoryParams}&strategy=${strategy}`;
 
